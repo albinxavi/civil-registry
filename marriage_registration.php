62bfe032417095a31263f34fe7260ddd784c84c7
Amal Antony <amalantonygit>
2023-08-15 22:10:01 +0530

    Updated input type

--- marriage_registration.php
+++ marriage_registration.php
@@ -59,7 +59,7 @@ include('services/user/session.php');
                <div style="display: flex;">
                     <div style="margin-right:20px ;">
                         
-                              <label>User ID  :</label><input  type = "text" <input readonly type="text" value=<?php echo $_SESSION['id']; ?> name = "user_id" class = "box"/><br /><br/>
+                              <label>User ID  :</label><input readonly type="text" value=<?php echo $_SESSION['id']; ?> name = "user_id" class = "box"/><br /><br/>
                               <label>Date of Marriage  :</label><input type = "date" name = "date_of_marriage" class = "box" /><br/><br/><br>
                               <div style="border: solid 1px; width:80%; margin: 10px; color:#333333; width:300px;">
                                 <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Bride detail</b></div>
