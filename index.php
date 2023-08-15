073eca4ed5af3aeb332ba8cc06a78a8ec7cb7867
Albin Xavier <albinxavierx@gmail.com>
2023-08-15 21:12:59 +0530

    login nav bar

--- index.php
+++ index.php
@@ -1,42 +1,47 @@
 <html>
-   <head>
-      <title>Login Page</title>
-      
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-            width:100%;
-         }
-      </style>
-      
-   </head>
-   
-   <body bgcolor = "#FFFFFF">
-	
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-				
-            <div style = "width:80%;margin:10%">
-               
-               <form action = "services/user/login.php" method = "post">
-                  <label>Username  :</label><input type = "text" name = "email" class = "box"/><br /><br/>
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br/>
-                  <div align = "center"><input type = "submit" value = " Submit "/><br/></div>
-               </form>
-               <div align = "center"><a href="registration.php">New User? Register Now</a></div>
-               
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px">
-               <?php
+  <head>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="./style.css" />
+    <style type="text/css">
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 14px;
+      }
+      label {
+        font-weight: bold;
+        width: 100px;
+        font-size: 14px;
+      }
+      .box {
+        border: #666666 solid 1px;
+        width: 100%;
+      }
+    </style>
+  </head>
+
+  <body bgcolor="#FFFFFF">
+    <nav>
+      <div class="menu">
+        <div class="logo">
+          <a href="">Civil Registry</a>
+        </div>
+      </div>
+    </nav>
+    <br /><br /><br /><br /><br />
+    <div align="center">
+      <div style="width: 300px; border: solid 1px #333333" align="left">
+        <div style="background-color: #333333; color: #ffffff; padding: 3px"><b>Login</b></div>
+
+        <div style="width: 80%; margin: 10%">
+          <form action="services/user/login.php" method="post">
+            <label>Username :</label><input type="text" name="email" class="box" /><br /><br />
+            <label>Password :</label><input type="password" name="password" class="box" /><br /><br />
+            <div align="center"><input type="submit" value=" Submit " /><br /></div>
+          </form>
+          <div align="center"><a href="registration.php">New User? Register Now</a></div>
+
+          <div style="font-size: 11px; color: #cc0000; margin-top: 10px">
+            <?php
                if (isset($_GET['register']))
                {
                   if($_GET['register']=='success'){
@@ -53,13 +58,9 @@
                   }
                }
                ?>
-            </div>
-					
-            </div>
-				
-         </div>
-			
+          </div>
+        </div>
       </div>
-
-   </body>
+    </div>
+  </body>
 </html>
