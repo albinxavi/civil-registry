73c5ce1db3025a396705521821d5889538a71e6c
Amal Antony <amalantonygit>
2023-08-15 20:21:53 +0530

    Modified file upload logic

--- home.php
+++ home.php
@@ -1,47 +1,48 @@
 <?php
-   include('services/user/session.php');
+include('services/user/session.php');
 ?>
 <!DOCTYPE html>
 
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Simple Website Design HTML CSS | CodingNepal</title>
-    <link rel="stylesheet" href="./style.css" />
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-  </head>
-  <body>
-    <nav>
+
+<head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Simple Website Design HTML CSS | CodingNepal</title>
+   <link rel="stylesheet" href="./style.css" />
+   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+<body>
+   <nav>
       <div class="menu">
-        <div class="logo">
-          <a href="/home.php">Civil Registry</a>
-        </div>
-        
-        <ul>
+         <div class="logo">
+            <a href="/home.php">Civil Registry</a>
+         </div>
+
+         <ul>
             <label style="color: aliceblue;">
-            <?php
-             if($_SESSION['admin']==1){
-              echo $_SESSION['name']."(Admin)";
-             }
-             else
-             {
-               echo $_SESSION['name'];
-             }
-             ?>
+               <?php
+               if ($_SESSION['admin'] == 1) {
+                  echo $_SESSION['name'] . "(Admin)";
+               } else {
+                  echo $_SESSION['name'];
+               }
+               ?>
             </label>
             <li><a href="services/user/logout.php">Logout</a></li>
-          </ul>
+         </ul>
       </div>
-    </nav>
-    <div class="img"></div>
-    <div class="center">
+   </nav>
+   <div class="img"></div>
+   <div class="center">
       <div class="title">Services</div>
       <div class="btns">
-        <button>Marriage Registration</button>
-        <a href="birth_registration.php" style="text-decoration:none;"><button>Birth Registration</button></a>
-        <a href="death_registration.php" style="text-decoration:none;"><button>Death Registration</button></a>
+         <button>Marriage Registration</button>
+         <a href="birth_registration.php" style="text-decoration:none;"><button>Birth Registration</button></a>
+         <a href="death_registration.php" style="text-decoration:none;"><button>Death Registration</button></a>
       </div>
-    </div>
-  </body>
+   </div>
+</body>
+
 </html>
--- services/user/session.php
+++ services/user/session.php
@@ -1,5 +1,5 @@
 <?php
-   include($_SERVER['DOCUMENT_ROOT']."\inc\config.php");
+   include($_SERVER['DOCUMENT_ROOT']."/inc/config.php");
    session_start();
    
    $user_check = $_SESSION['login_user'];
